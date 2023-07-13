43d0d9bfdbc7ee0df38f5df70d0e1b13555ab0f1
Mickie <mickystn10@gmail.com>
2023-07-14 02:26:10 +0700

    Update api.jsx

--- frontend/src/service/api.jsx
+++ frontend/src/service/api.jsx
@@ -1,6 +1,6 @@
 
 import Axios from 'axios';
-const api = "http://localhost:3001"
+const api = "https://liongate-backend.vercel.app"
 const headers = { 'Authorization': 'Bearer liongate'}; // 
 
 export const register = async (data)=>{
